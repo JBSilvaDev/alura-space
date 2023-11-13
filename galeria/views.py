cf1586fda2a852c8ae49427b364e6deb3a980f36
JB Silva Dev <jbsilva.dev@outlook.com>
2023-11-13 14:30:11 -0300

    presistencia dee dados

--- galeria/views.py
+++ galeria/views.py
@@ -17,4 +17,12 @@ def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, "galeria/imagem.html", {"foto":foto})
 
+def buscar(request):
+  fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+  if "buscar" in request.GET:
+     nome_buscado = request.GET['buscar']
+     if nome_buscado:
+        fotografias = fotografias.filter(nome__icontains=nome_buscado)
+  return render(request, 'galeria/buscar.html', {"cards": fotografias})
+
 
