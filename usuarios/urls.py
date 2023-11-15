a460b82bb4bd2a312dc2f4a04d628aaec4eead06
JB Silva Dev <jbsilva.dev@outlook.com>
2023-11-14 21:29:56 -0300

    inserindo alertas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from usuarios.views import login, cadastro, logout
 
 
 urlpatterns = [
-    path("login", login, name='login'),
-    path("cadastro", cadastro, name='cadastro'),
-    path("logout", logout, name='logout')
+    path("login", login, name="login"),
+    path("cadastro", cadastro, name="cadastro"),
+    path("logout", logout, name="logout"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,5 @@
-from ast import If
 from django.shortcuts import redirect, render
-from django.urls import is_valid_path
+
 
 from usuarios.forms import CadastroForms, LoginForms
 from django.contrib.auth.models import User
@@ -10,57 +9,57 @@ from django.contrib import messages
 
 # Create your views here.
 def login(request):
-  form = LoginForms()
-  if request.method == 'POST':
-    form = LoginForms(request.POST)
-    if form.is_valid():
-      nome = form['nome_login'].value()
-      senha = form['senha'].value()
-    
-    usuario = auth.authenticate(
-      request,
-      username = nome,
-      password = senha,
-    )
-    if usuario is not None:
-      auth.login(request, usuario)
-      messages.success(request, f"Bem vindo {nome}!")
-      return redirect('home')
-    else:
-      messages.error(request, "Usuário ou senha inválidos")
-      return redirect('login')
+    form = LoginForms()
+    if request.method == "POST":
+        form = LoginForms(request.POST)
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
 
-  return render(request, "usuarios/login.html",{"form":form})
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha,
+        )
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f"Bem vindo {nome}!")
+            return redirect("home")
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+            return redirect("login")
+
+    return render(request, "usuarios/login.html", {"form": form})
+
 
 def cadastro(request):
-  form = CadastroForms()
+    form = CadastroForms()
 
-  if request.method == 'POST':
-    form = CadastroForms(request.POST)
-    if form.is_valid():
-      if form['senha_1'].value() != form['senha_2'].value():
-        messages.error(request, "Senhas não são iguais")
-        return redirect('cadastro')
-      nome=form['nome_cadastro'].value()
-      email=form['email'].value()
-      senha_1=form['senha_1'].value()
+    if request.method == "POST":
+        form = CadastroForms(request.POST)
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais")
+                return redirect("cadastro")
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha_1 = form["senha_1"].value()
 
-      if User.objects.filter(username=nome).exists():
-        messages.error(request, f"Usuário {nome} não está disponível!")
-        return redirect('cadastro')
-      
-      usuario = User.objects.create_user(
-        username=nome,
-        email=email,
-        password=senha_1
-      )
-      usuario.save()
-      messages.success(request, f"Cadastro realizado com sucesso -> {nome}!")
-      return redirect('login')
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, f"Usuário {nome} não está disponível!")
+                return redirect("cadastro")
 
-  return render(request, "usuarios/cadastro.html",{"form":form})
-  
+            usuario = User.objects.create_user(
+                username=nome, email=email, password=senha_1
+            )
+            usuario.save()
+            messages.success(request, f"Cadastro realizado com sucesso -> {nome}!")
+            return redirect("login")
+
+    return render(request, "usuarios/cadastro.html", {"form": form})
+
+
 def logout(request):
-  auth.logout(request)
-  messages.info(request, "Você saiu...")
-  return redirect("login")
+    auth.logout(request)
+    messages.info(request, "Você saiu...")
+    return redirect("login")
