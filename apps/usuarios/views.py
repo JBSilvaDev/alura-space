054bf64edba14044b2c0da1628c08408f749243e
JB Silva Dev <jbsilva.dev@outlook.com>
2023-11-16 19:53:36 -0300

    refatorando projeto

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 
-from usuarios.forms import CadastroForms, LoginForms
+from apps.usuarios.forms import CadastroForms, LoginForms
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
